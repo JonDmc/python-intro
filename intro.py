6f9aab63fe966e9f1f3ec4f0f65f357d6e20fcf7
Jon Dimaculangan <jondmclngn@gmail.com>
2023-02-08 23:20:33 -0600

    intro to python - Data types

---
+++ intro.py
@@ -0,0 +1,47 @@
+# this is a comment -- will not be executed
+
+#multi
+#line
+#comment
+
+"""
+this is a doc string -- we will learn more about this kind of special comment later
+-more than one line
+"""
+
+# variables use 'snake_case' -- lowercase and spaces get underscores
+my_var = 'hello snakes!'
+
+print(my_var)
+
+"""
+How to run the file:
+-> pyhton3 <filename.py>
+"""
+# ## ## ## ## ## #
+
+# DATA TYPES
+
+# Bools
+
+None # falsey -- like null and undefined all in one
+True # a truthy bool
+False # a Falsey bool
+
+# Text type
+str # string constructor
+# literals
+my_string = "double quotes are cool"
+my_other_string = 'single quotes are fine too'
+
+# Number types 
+# integers -- whole numbers
+int
+my_int = 30
+# floating points or floats -- decimals
+float
+my_float = 1.0123123
+# complex nums
+complex
+my_complex = 3j
+my_other_complex = 2j
